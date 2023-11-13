0a5a479e98431d02b5386f469f004b7e4585208d
mmubashirmunir <mubashir.intern@devsinc.com>
2023-11-13 17:03:57 +0500

    decrement

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _decerement() {
     setState(() {
       _counter--;
     });
@@ -81,8 +81,8 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _decerement,
+        tooltip: 'Decrement',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
